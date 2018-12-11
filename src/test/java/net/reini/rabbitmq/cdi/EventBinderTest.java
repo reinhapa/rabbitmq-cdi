76ff93bdbb8455840174d1f7667e17de77db42ff
Patrick Reinhart <patrick@reini.net>
2018-12-11 23:40:40 +0100

    Adds test for EventBinder basic methods

---
+++ src/test/java/net/reini/rabbitmq/cdi/EventBinderTest.java
@@ -0,0 +1,79 @@
+package net.reini.rabbitmq.cdi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+
+import javax.enterprise.event.Event;
+import javax.enterprise.inject.Instance;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import net.reini.rabbitmq.cdi.EventBinder.ExchangeBinding;
+import net.reini.rabbitmq.cdi.EventBinder.QueueBinding;
+
+@ExtendWith(MockitoExtension.class)
+class EventBinderTest {
+  @Mock
+  private Event<Object> remoteEventControl;
+  @Mock
+  private Instance<Object> remoteEventPool;
+  @Mock
+  private EventPublisher eventPublisher;
+  @Mock
+  private ConnectionProducer connectionProducer;
+
+  @InjectMocks
+  private TestEventBinder eventBinder;
+
+  @BeforeEach
+  void prepare() {
+    eventBinder.initializeConsumerContainer();
+  }
+
+  @Test
+  void testBind() {
+    assertNotNull(eventBinder.bind(TestEvent.class));
+  }
+
+  @Test
+  void testConfiguration() {
+    assertNotNull(eventBinder.configuration());
+  }
+
+  @Test
+  void testInitialize() throws IOException {
+    eventBinder.initialize();
+  }
+
+  @Test
+  void testBindQueue() {
+    QueueBinding<TestEvent> queueBinding = new QueueBinding<>(TestEvent.class, "queue");
+
+    eventBinder.bindQueue(queueBinding);
+  }
+
+  @Test
+  void testBindExchange() {
+    ExchangeBinding<TestEvent> exchangeBinding = new ExchangeBinding<>(TestEvent.class, "exchange");
+
+    eventBinder.bindExchange(exchangeBinding);
+  }
+
+  @Test
+  void testUriDecode() {
+    assertEquals("stock + stein", EventBinder.uriDecode("stock%20+%20stein"));
+  }
+
+
+  static class TestEventBinder extends EventBinder {
+    @Override
+    protected void bindEvents() {}
+  }
+}
