05acca4991bce670e7db9746095a4cc4ce267d8a
Patrick Reinhart <patrick@reini.net>
2018-12-12 21:53:00 +0100

    Enhances tests for adding headers

--- src/test/java/net/reini/rabbitmq/cdi/ExchangeBindingTest.java
+++ src/test/java/net/reini/rabbitmq/cdi/ExchangeBindingTest.java
@@ -1,7 +1,7 @@
 package net.reini.rabbitmq.cdi;
 
 import static com.rabbitmq.client.MessageProperties.BASIC;
-import static com.rabbitmq.client.MessageProperties.PERSISTENT_TEXT_PLAIN;
+import static com.rabbitmq.client.MessageProperties.PERSISTENT_BASIC;
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.BiConsumer;
@@ -19,6 +20,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.rabbitmq.client.AMQP.BasicProperties;
+
 import net.reini.rabbitmq.cdi.EventBinder.ExchangeBinding;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,10 +31,12 @@ class ExchangeBindingTest {
   @Mock
   private BiConsumer<TestEvent, PublishException> errorHandler;
 
+  private BasicProperties basicProperties;
   private ExchangeBinding<TestEvent> binding;
 
   @BeforeEach
   void prepare() {
+    basicProperties = BASIC.builder().headers(emptyMap()).build();
     binding = new ExchangeBinding<>(TestEvent.class, "exchange");
   }
 
@@ -61,12 +66,14 @@ class ExchangeBindingTest {
 
   @Test
   void withProperties() {
-    Map<String, Object> headers = emptyMap();
-    assertEquals(BASIC.builder().headers(headers).build(),
-        binding.getBasicPropertiesBuilder().build());
-    assertSame(binding, binding.withProperties(PERSISTENT_TEXT_PLAIN));
-    assertEquals(PERSISTENT_TEXT_PLAIN.builder().headers(headers).build(),
-        binding.getBasicPropertiesBuilder().build());
+    assertEquals(basicProperties, binding.getBasicPropertiesBuilder().build());
+    BasicProperties properties = PERSISTENT_BASIC.builder().headers(emptyMap()).build();
+    assertEquals(emptyMap(), properties.getHeaders());
+    assertSame(binding, binding.withProperties(properties));
+    assertEquals(properties, binding.getBasicPropertiesBuilder().build());
+
+    assertSame(binding, binding.withProperties(properties.builder().headers(null).build()));
+    assertEquals(properties, binding.getBasicPropertiesBuilder().build());
   }
 
   @Test
@@ -81,10 +88,22 @@ class ExchangeBindingTest {
   @Test
   void withHeader() {
     Object headerValue = new Object();
-    assertEquals(BASIC.builder().headers(emptyMap()).build(),
-        binding.getBasicPropertiesBuilder().build());
+    assertEquals(basicProperties, binding.getBasicPropertiesBuilder().build());
     assertSame(binding, binding.withHeader("header", headerValue));
-    assertEquals(BASIC.builder().headers(singletonMap("header", headerValue)).build(),
+    BasicProperties expected = BASIC.builder().headers(singletonMap("header", headerValue)).build();
+    assertEquals(expected, binding.getBasicPropertiesBuilder().build());
+  }
+
+  @Test
+  void withHeader_preserving_existing_headers() {
+    BasicProperties properties =
+        PERSISTENT_BASIC.builder().headers(singletonMap("oldheader", "oldvalue")).build();
+    binding.withProperties(properties);
+    assertSame(binding, binding.withHeader("header", "value"));
+    Map<String, Object> expectedHeaders = new HashMap<>();
+    expectedHeaders.put("oldheader", "oldvalue");
+    expectedHeaders.put("header", "value");
+    assertEquals(PERSISTENT_BASIC.builder().headers(expectedHeaders).build(),
         binding.getBasicPropertiesBuilder().build());
   }
 
@@ -98,5 +117,23 @@ class ExchangeBindingTest {
   void testHashCode() {
     assertEquals(Objects.hash(TestEvent.class, "exchange"), binding.hashCode());
   }
-}
 
+  @Test
+  void testEquals() {
+    assertNotEquals(binding, null);
+    assertNotEquals(binding, new Object());
+    assertNotEquals(binding, new ExchangeBinding<>(Object.class, "exchange"));
+    assertNotEquals(binding, new ExchangeBinding<>(TestEvent.class, "exchangeX"));
+
+    assertEquals(binding, binding);
+
+    ExchangeBinding<TestEvent> binding1 = new ExchangeBinding<>(TestEvent.class, "exchange");
+    assertEquals(binding, binding1);
+    assertEquals(binding1, binding);
+
+    ExchangeBinding<TestEvent> binding2 = new ExchangeBinding<>(TestEvent.class, "exchange");
+    assertEquals(binding, binding2);
+
+    assertEquals(binding1, binding1);
+  }
+}
