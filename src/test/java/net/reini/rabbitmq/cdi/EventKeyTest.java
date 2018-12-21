a8ac4db1ac84f8741e23195dc99c75c45ff25fcf
Patrick Reinhart <patrick@reini.net>
2018-12-21 15:05:53 +0100

    Fixed static import

--- src/test/java/net/reini/rabbitmq/cdi/EventKeyTest.java
+++ src/test/java/net/reini/rabbitmq/cdi/EventKeyTest.java
@@ -1,7 +1,7 @@
 package net.reini.rabbitmq.cdi;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import javax.enterprise.event.TransactionPhase;
 
