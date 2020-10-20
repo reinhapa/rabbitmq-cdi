39050ba6baeec41b4865168c80e440e7a6fa0572
Patrick Reinhart <patrick@reini.net>
2020-10-20 08:30:46 +0200

    Adds single static import for assertEquals

--- src/test/java/net/reini/rabbitmq/cdi/ConsumerHolderTest.java
+++ src/test/java/net/reini/rabbitmq/cdi/ConsumerHolderTest.java
@@ -25,6 +25,7 @@
 package net.reini.rabbitmq.cdi;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
