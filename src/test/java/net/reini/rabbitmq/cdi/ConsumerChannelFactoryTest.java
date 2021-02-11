3e3967d5797e95415945182675d49312315c49bd
Patrick Reinhart <patrick@reini.net>
2021-02-11 22:24:48 +0100

    updates kordamp, gradle and libraries

--- src/test/java/net/reini/rabbitmq/cdi/ConsumerChannelFactoryTest.java
+++ src/test/java/net/reini/rabbitmq/cdi/ConsumerChannelFactoryTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015, 2019 Patrick Reinhart
+ * Copyright (c) 2015-2020 Patrick Reinhart
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package net.reini.rabbitmq.cdi;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
